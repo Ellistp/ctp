1d59c8b38c1286eb1519c7c91b850879352a0120
chengtianping <1653493637@qq.com>
2018-05-12 17:38:30 +0800

    项目结构 调整

--- ghub-web/src/main/java/com/ctp/ghub/controller/UserController.java
+++ ghub-web/src/main/java/com/ctp/ghub/controller/UserController.java
@@ -1,8 +1,24 @@
 package com.ctp.ghub.controller;
 
+import com.ctp.ghub.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Administrator on 2018/5/12 0012.
  */
+@Controller
+@RequestMapping("/api/ghub/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/getUser",method = RequestMethod.GET)
+    public String getUser(){
+        return "chengtianping";
+    }
 }
