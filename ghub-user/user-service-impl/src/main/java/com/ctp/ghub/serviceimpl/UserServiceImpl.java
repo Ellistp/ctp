238ed9961a0419d13bd4924715d865f736fcf8c7
wb-ctp278982 <wb-ctp278982@alibaba-inc.com>
2018-05-14 13:50:29 +0800

    项目结构调整

--- ghub-user/user-service-impl/src/main/java/com/ctp/ghub/serviceimpl/UserServiceImpl.java
+++ ghub-user/user-service-impl/src/main/java/com/ctp/ghub/serviceimpl/UserServiceImpl.java
@@ -1,11 +1,12 @@
-package com.ctp.ghub;
+package com.ctp.ghub.serviceimpl;
 
+import com.ctp.ghub.UserService;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2018/5/12 0012.
  */
 @Service("userService")
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 }
