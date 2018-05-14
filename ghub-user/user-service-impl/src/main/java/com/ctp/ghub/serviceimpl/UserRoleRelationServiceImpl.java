cdf39ee63813f2e27ac59920e31af30576b7708e
wb-ctp278982 <wb-ctp278982@alibaba-inc.com>
2018-05-14 14:25:39 +0800

    项目结构调整

---
+++ ghub-user/user-service-impl/src/main/java/com/ctp/ghub/serviceimpl/UserRoleRelationServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ctp.ghub.serviceimpl;
+
+import com.ctp.ghub.UserRoleRelationService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2018/5/12 0012.
+ */
+@Service("userRoleRelationService")
+public class UserRoleRelationServiceImpl implements UserRoleRelationService {
+}
