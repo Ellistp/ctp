96df2c44fec0a48d84b7aaebf5d85e4093491ab8
wb-ctp278982 <wb-ctp278982@alibaba-inc.com>
2018-05-14 14:50:51 +0800

    整体结构 -- 完成

--- ghub-web/src/main/java/com/ctp/ghub/controller/UserController.java
+++ ghub-web/src/main/java/com/ctp/ghub/controller/UserController.java
@@ -3,9 +3,9 @@ package com.ctp.ghub.controller;
 import com.ctp.ghub.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2018/5/12 0012.
@@ -18,6 +18,7 @@ public class UserController {
     UserService userService;
 
     @RequestMapping(value = "/getUser",method = RequestMethod.GET)
+    @ResponseBody
     public String getUser(){
         return "chengtianping";
     }
