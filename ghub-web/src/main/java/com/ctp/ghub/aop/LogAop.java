d60c270ac7431032321e88dfde394effa3750d33
wb-ctp278982 <wb-ctp278982@alibaba-inc.com>
2018-05-22 10:56:55 +0800

    项目 集成aop

--- ghub-web/src/main/java/com/ctp/ghub/aop/LogAop.java
+++ ghub-web/src/main/java/com/ctp/ghub/aop/LogAop.java
@@ -1,7 +1,16 @@
 package com.ctp.ghub.aop;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,5 +22,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAop {
     private static final Logger logger = Logger.getLogger(LogAop.class);
+
+    @Pointcut("within(@org.springframework.stereotype.Controller *)")
+    public void controller() {
+    }
+
+    @Pointcut("execution(* *.*(..))")
+    protected void allMethod() {
+    }
+
+    @Before("controller() && allMethod() && args(..,request)")
+    public void logBefore(JoinPoint joinPoint, HttpServletRequest request) throws Throwable {
+        if (null != request) {
+
+        }
+    }
+
+    @AfterReturning(pointcut = "controller() && allMethod()", returning = "result")
+    public void afterControllerMethod(JoinPoint joinPoint, Object result) {
+
+    }
+
+    @AfterThrowing(pointcut = "controller() && allMethod()", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+
+    }
+
+    @Around("controller() && allMethod()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        return null;
+    }
 
 }
